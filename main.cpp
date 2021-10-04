b5ea95422bc085e54d9eab84b05f21215b1f7b85
Igor Makarevich <igor.makarevich@novometgroup.com>
2021-10-04 19:25:12 +0500

    checking file open was added (is_open method)

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 int main() {
     std::ifstream words;
     words.open("..\\words.txt");
+    if (!words.is_open()) {
+        std::cout << "Open file error!";
+        return 666;
+    }
     std::string temp;
     int num = 0;
     while (!words.eof()) {
